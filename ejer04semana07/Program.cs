6aba7954865974c9fcb2c4dc529a90de924c537e
Sandy Solis <sandysolis0305@gmail.com>
2023-10-04 17:32:38 -0500

    Agregar archivos de proyecto.

---
+++ ejer04semana07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer04semana07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Ingresa un número: ");
+                int num=int.Parse(Console.ReadLine());
+                for (int j = 0; j < num; j++)
+                {
+                    for (int k = 0; k < num*2; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
